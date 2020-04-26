03d6d99bb026882fd9887f3a71b886e3fc166490
Dmytro O <mr.oda.ua@gmail.com>
2020-04-27 01:00:53 +0300

    HW O2 proceed task01

--- js/task01.js
+++ js/task01.js
@@ -1,27 +1,46 @@
-/* eslint-disable no-console */
-// eslint-disable prefer-const
-// eslint-disable no-unused-vars
+// Напиши функцию logItems(array), которая получает массив и использует цикл for, который для каждого элемента массива будет выводить в консоль сообщение в формате [номер элемента] - [значение элемента].
+
+// Нумерация должна начинаться с 1. К примеру для первого элемента массива ['Mango', 'Poly', 'Ajax'] с индексом 0 будет выведено '1 - Mango', а для индекса 2 выведет '3 - Ajax'.
 
 // ====---- Task 01 Start
-console.log('TASK 01');
 
-let name = 'Генератор защитного поля ';
-let price = 1000;
+const namesArray = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
 
-console.log(
-  'Выбран ', 
-  name , 
-  'цена за штуку',
-  price,
-  ' кредитов'
-);
+const logItems = function(array) {
+  for (let i = 0; i < namesArray.lenght +1; i += 1); {
+    console.log('0', i, ' - ', namesArray[i]);
+  }
+};
 
-price = 2000;
+const result = logItems(namesArray);
 
-console.log(
-  'Выбран ', 
-  name , 
-  'цена за штуку',
-  price,
-  ' кредитов'
-);
+console.log(namesArray);
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+
+
+
+// console.log('TASK 01');
+
+// let name = 'Генератор защитного поля ';
+// let price = 1000;
+
+// console.log(
+//   'Выбран ', 
+//   name , 
+//   'цена за штуку',
+//   price,
+//   ' кредитов'
+// );
+
+// price = 2000;
+
+// console.log(
+//   'Выбран ', 
+//   name , 
+//   'цена за штуку',
+//   price,
+//   ' кредитов'
+// );
